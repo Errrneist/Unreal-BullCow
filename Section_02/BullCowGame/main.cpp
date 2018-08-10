692731758f180aa4cf8903d591f2c061f3bb1a65
Hongjun Wu <hongjunwu@hotmail.com>
2018-08-10 09:56:04 +0800

    Developed more of the bull cow game

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -1,3 +1,19 @@
+/* 
+Hongjun Wu
+20180710
+A program that plays the bull and cow game.
+*/
+
+#include <iostream> // Include standard stuff
+
+using namespace std;
+
 int main(){
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to bulls and cows, a fun word game!" << endl;
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I am thinking of?\n";
 	return 0;
 }
+
+
